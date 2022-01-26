28c861dfee5740266c3376485933018f8b965221
zap <zap@hucr.cz>
2022-01-26 15:47:48 +0100

    theme initialization

--- controllers/Galleries.php
+++ controllers/Galleries.php
@@ -3,15 +3,15 @@
 use Backend\Classes\Controller;
 use BackendMenu;
 
-class Gallery extends Controller
+class Galleries extends Controller
 {
     public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController'    ];
-    
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
 
     public $requiredPermissions = [
-        'all' 
+        'all'
     ];
 
     public function __construct()
